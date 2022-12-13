c7b0193befdeccdfa956d03ee0667d4e4d74f12c
jahids <evanjahid321@gmail.com>
2022-12-13 17:29:05 +0600

    all working

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,16 +1,25 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useMatch, useNavigate } from "react-router-dom";
 import { Searched } from "../../features/filter/filterSlice";
 
 export default function Search() {
     const dispatch = useDispatch();
     const { search } = useSelector((state) => state.filter);
-
     const [input, setInput] = useState(search);
+
+    const match = useMatch("/")
+    const navigate = useNavigate();
+    console.log(match);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         dispatch(Searched(input))
+
+        // if user is not in home page, redirect to home page
+        if (!match) {
+            navigate('/')
+        }
 
     }
     return (
